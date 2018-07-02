91d4c8eeef5cf02f6524bcc89cc6bd55a3ef36d5
Anastasia <anastasia.diudina@gmail.com>
2018-07-02 15:46:49 +0300

    Load categories and categorised budgets on app opening

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux';
 import configureStore from 'src/configure-store';
 import { getCurrentUser } from 'src/actions/user';
 import { getCategories } from 'src/actions/categories';
+import { getCategorisedBudgets } from 'src/actions/categorisedBudgets';
 import Navigation from 'src/containers/Navigation';
 
 // TODO: populate information about budgets, user, categories etc.
@@ -10,6 +11,7 @@ import Navigation from 'src/containers/Navigation';
 const store = configureStore()
 store.dispatch(getCurrentUser())
 store.dispatch(getCategories())
+store.dispatch(getCategorisedBudgets())
 
 export default class App extends Component {
   render() {
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,13 @@ import { combineReducers } from 'redux';
 import user from 'src/reducers/user';
 import totalBudget from 'src/reducers/totalBudget';
 import navigation from 'src/reducers/navigation';
+import categories from 'src/reducers/categories';
+import categorisedBudgets from 'src/reducers/categorisedBudgets';
 
 export default combineReducers({
   user,
   totalBudget,
-  navigation
+  navigation,
+  categories,
+  categorisedBudgets,
 })
